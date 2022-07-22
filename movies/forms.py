49df21539238a39802968f39c2c67e787e58c79d
ll_admin <isaac.fatokun@yahoo.com>
2022-07-22 18:34:46 +0200

    Small updates
    Isaac

--- movies/forms.py
+++ movies/forms.py
@@ -1,9 +1,13 @@
 from django import forms
 from .models import Movie
 
+from moviepy.editor import VideoFileClip
+
 # define the class of a form
 
 class MovieForm(forms.ModelForm):
     class Meta:
         model = Movie
         fields = {'title' : '','video' : ''}
+
+     # Add some custom validation to our image field
